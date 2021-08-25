21c6e1404de853007f9c2f484caccd2f408e7a29
Vic <vjuska24@gmail.com>
2021-08-25 09:56:37 -0500

    Created 3 products using db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.create(name: "Harvest Moon", price: 15.00, image_url: "https://images.squarespace-cdn.com/content/v1/5daa3d54e0f35e4910d94559/1612052593147-TW0MTXKVLQHBD3IUL495/IMG_2444.jpg?format=2500w", description: "Harvest Moon Matte Paste is a smooth and creamy paste that will offer texture and volume to your hair while maintaining a firm hold and matte finish throughout the day.")
+product = Product.create(name: "Frost Moon", price: 17.00, image_url: "https://images.squarespace-cdn.com/content/v1/5daa3d54e0f35e4910d94559/1602095624698-4OV7AWBCJ7RSOI6S1UF0/Frost+Moon+1.jpg?format=2500w", description: "Frost Moon is an Oil Based Clay that is packed with bentonite clay to provide texture and volume while bringing down the shine to a natural finish. ")
+product = Product.create(name: "Hunter's Moon", price: 17.00, image_url: "https://images.squarespace-cdn.com/content/v1/5daa3d54e0f35e4910d94559/1602095521006-FDVWN9ZWAYIAO5VTSNEF/Hunters+Moon+1.jpg?format=2500w", description: "Hunter’s Moon is a lotion like Texture Cream that provides coarse texture or fine texture depending on your preference.")
