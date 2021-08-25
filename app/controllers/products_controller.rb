d751e39ba1d2b93980fb6352af34a024c9cc6bfd
Vic <vjuska24@gmail.com>
2021-08-25 10:06:42 -0500

    Created a route that displays JSON for all the available products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def all_products
+    product = Product.all
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/all_products", controller: products, action: all_products
+  get "/harvest_moon", controller: products, action: harvest_moon
+  get "/frost_moon", controller: products, action: frost_moon
+  get "/hunters_moon", controller: products, action: hunters_moon
 end
