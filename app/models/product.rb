41320240c6db18a3d49d87a998d72c2b39289324
Vic <vjuska24@gmail.com>
2021-08-31 11:26:37 -0500

    add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, length: { in: 5..500 }
+
   def is_discounted?
     if price <= 10 #Could also write lines 3-7 as just price <= 10
       return true
