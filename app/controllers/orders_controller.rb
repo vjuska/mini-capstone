dea6e8c859a0fefda41d73948383f9e84de4c033
Vic <vjuska24@gmail.com>
2021-09-07 14:47:56 -0500

    Update orders_controller.rb and requests.http

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,34 @@
 class OrdersController < ApplicationController
+  def index
+    orders = current_user.orders
+    render json: orders
+  end
+
   def create
     product = Product.find_by(id: params["product_id"])
+
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
     order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       # image_url: params[:input_url],
       quantity: params[:quantity],
-      subtotal: product.price * params[:quantity].to_i,
-      tax: product.tax * params[:quantity].to_i,
-      total: (product.price * params[:quantity].to_i) + (product.tax * params[:quantity].to_i),
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
-    order.save
-    render json: order
+    if order.save
+      render json: order
+    else
+      render json: { errors: order.errors.full_messages }, status: 422
+    end
   end
 
   def show
     order = Order.find_by(id: params["id"], user_id: current_user.id) #In your mini-capstone, create a single route that can display ANY single product using a query parameter.
     render json: order
-  end
-
-  def index
-    orders = current_user.orders
-    render json: orders
   end
 end
